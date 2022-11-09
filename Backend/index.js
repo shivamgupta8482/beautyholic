c67f36a1fde840cbf3cf284f928a6c8fa7b970b3
Imran Khan <99397606+imran120198@users.noreply.github.com>
2022-11-09 10:27:05 +0530

    established connection to database

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,25 @@
-console.log("1");
+const express = require("express")
+const cors = require("cors")
+
+const {connection} = require("./Config/db")
+require("dotenv").config()
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/",(req,res) => {
+    res.send("WELCOME TO BEAUTYHOLIC SERVER")
+})
+
+app.listen(process.env.PORT, async () => {
+    try {
+      await connection;
+      console.log("Connected To Database");
+    } catch (err) {
+      console.log("Error is Connecting Database");
+      console.log(err);
+    }
+    console.log(`Server is listening at PORT ${process.env.PORT}`);
+  });
